c0ae9d6a479ba9ac9a947b03e9dd4715102c5a0b
KitaharaMugiro <ngthornym@yahoo.co.jp>
2019-05-18 11:54:46 +0900

    Initial Commit

---
+++ yumemi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yumemi
+//
+//  Created by kitaharamugirou on 2019/05/18.
+//  Copyright © 2019 kitaharamugirou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
